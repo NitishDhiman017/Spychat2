cdbf816017ab389b6c062305f28b8d89672e8896
Nitish Dhiman <nitishdhiman374@gmail.com>
2017-07-10 22:42:50 +0530

    own post

---
+++ constant.py
@@ -0,0 +1,7 @@
+#global var files
+
+#instagram api access token
+APP_ACCESS_TOKEN= '2311061833.3f5712f.e0d7c10620fd459f81103080fcbc72c9'
+
+#instagram api base url
+BASE_URL= 'https://api.instagram.com/v1/'
